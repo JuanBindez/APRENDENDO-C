4c2db137e2e4e195260168568c382ec4cc3c37c1
Juan Bindez <juanbindez780@gmail.com>
2025-09-05 22:49:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    printf("Olá mundo!\n");
+    float valor_de_compra;
+    float valor_liquido;
+    float valor_de_frete;
+    
+    int qtd_pessoas;
+    
+    printf("\n Digite o valor total da compra: ");
+    
+    scanf("%f", &valor_de_compra);
+    
+    printf("\n Digite a quantidade de pessoas: ");
+    
+    scanf("%d", &qtd_pessoas);
+    
+    printf("\n Digite o valor do frete: ");
+    
+    scanf("%f", &valor_de_frete);
+    
+    valor_liquido = valor_de_compra + valor_de_frete;
+    
+    printf("\n Valor da conta com frete = %f", valor_liquido);
+    
+    printf("\n Valor a ser pago por pessoa = ");
+    
+    printf("%f", valor_liquido / qtd_pessoas);
+    
+    return 0;
 }
